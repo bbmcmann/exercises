805a77fe930ab21bba3a55e36fbfcdb20ce60f29
Ben McMann <bbmcmann@volosys.com>
2019-08-27 12:38:35 -0600

    Finish program

--- chapter13/BrickDistance.java
+++ chapter13/BrickDistance.java
@@ -4,7 +4,14 @@ public class BrickDistance{
 
     public static void main(String[] args){
 
+        Scanner input = new Scanner(System.in);
+        double g = 32.174;
+        double time;
         
+        System.out.println("Enter the time: ");
+        time = input.nextDouble();
+
+        System.out.println("Distance: " + (0.5 * g * (time*time)));
     }
 
 }
