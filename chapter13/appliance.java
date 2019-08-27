97016b891f4c9f7f13fb74fb21eb08ffb6aab519
Ben McMann <bbmcmann@volosys.com>
2019-08-27 12:33:00 -0600

    Finish program

--- chapter13/appliance.java
+++ chapter13/appliance.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
 
-public class appliance{
+public class Appliance{
 
     public static void main(String[] args){
 
-        
+        Scanner input = new Scanner(System.in);        
+        double cost, hours;
+
+        System.out.println("Enter the cost of kw per hour: ");
+        cost = input.nextDouble();
+
+        System.out.println("Enter the kw hours used this year: ");
+        hours = input.nextDouble();
+
+        System.out.println("Annual cost: " + (cost*hours)/100);
 
     } 
 
