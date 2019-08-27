3b88bb81b97ca4f3511472c583ccdb66326ab25c
Ben McMann <bbmcmann@volosys.com>
2019-08-27 12:46:55 -0600

    Finish Program

--- chapter13/LogTwo.java
+++ chapter13/LogTwo.java
@@ -4,8 +4,16 @@ public class LogTwo{
 
     public static void main(String[] args){
 
+        Scanner input = new Scanner(System.in);
+        double num, baseTwo;
 
-        
+        System.out.println("Enter a double: ");
+        num = input.nextDouble();
+
+        baseTwo = (Math.log(num)/(Math.log(2)));
+
+        System.out.println("Base 2 log of the number is: " + baseTwo);
+
     }
 
 }
