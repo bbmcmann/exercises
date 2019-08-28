3a0fa8d41dd69c114026e7db1a4c58a0c691d601
Ben McMann <bbmcmann@volosys.com>
2019-08-28 08:55:49 -0600

    Finish program

--- chapter16/AgeInSeconds.java
+++ chapter16/AgeInSeconds.java
@@ -1,11 +1,67 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class AgeInSeconds{
 
     public static void main(String[] args){
 
+        Scanner input = new Scanner(System.in);
+        int years, months, days, totalDays, totalSeconds;
+        double twoBill = 2500000000.0;
 
-        
+        //31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+        System.out.println("Enter the number of years: ");
+        years = input.nextInt();
+
+        System.out.println("Enter the months: ");
+        months = input.nextInt();
+
+        System.out.println("Enter the days: ");
+        days = input.nextInt();
+
+        totalDays = days + (years * 365);
+
+        if (months == 1){
+            totalDays += 31;
+        } 
+        else if (months == 2){
+            totalDays += (31 + 28);
+        }
+        else if (months == 3){
+            totalDays += (31 + 28 + 31);
+        }
+        else if (months == 4){
+            totalDays += (31 + 28 + 31 + 30);
+        }
+        else if (months == 5){
+            totalDays += (31 + 28 + 31 + 30 + 31);
+        }
+        else if (months == 6){
+            totalDays += (31 + 28 + 31 + 30 + 31 + 30);
+        }
+        else if (months == 7){
+            totalDays += (31 + 28 + 31 + 30 + 31 + 30 + 31);
+        }
+        else if (months == 8){
+            totalDays += (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31);
+        }
+        else if (months == 9){
+            totalDays += (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30);
+        }
+        else if (months == 10){
+            totalDays += (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31);
+        }
+        else if (months == 11){
+            totalDays += (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30);
+        }
+        else if (months == 12){
+            totalDays += (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31);
+        }
+
+        System.out.println("Total days alive: " + totalDays);
+        totalSeconds = totalDays * 24 * 60 * 60;
+        System.out.println("Total seconds alive: " + totalSeconds);
+        System.out.println("That is " + (((double)totalSeconds/twoBill) * 100) + " percent of your life!");
+
     }
 
 }
