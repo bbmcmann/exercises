f440ea59a65acc67d9e3e5f5819dbc7c4c9d81d0
Ben McMann <bbmcmann@volosys.com>
2019-09-06 12:27:45 -0600

    Finish

--- chapter16/Discount.java
+++ chapter16/Discount.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
 
 public class Discount{
-
     public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        int ammount;
 
+        System.out.println("Enter the purchase ammount");
+        ammount = input.nextInt();
 
+        System.out.println("Discount ammount: " + ammount*0.9);
 
     }
 
