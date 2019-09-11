274d1aff96a2408dcb9f0513927bf30fe893fdbe
Ben McMann <bbmcmann@volosys.com>
2019-09-11 13:08:21 -0600

    Initialize vars

--- chapter17/Internet.java
+++ chapter17/Internet.java
@@ -1,8 +1,16 @@
-public class Internet{
+import java.util.Scanner;
+import java.util.scanner;
 
+public class Internet{
     public static void main(String[] args){
 
-
+        Scanner input = new Scanner(System.in);
+        String order;
+        double price;
+        int delivery;
+        order = input.nextLine();
+        price = input.nextDouble();
+        delivery = input.nextInt();
         
     }
 
