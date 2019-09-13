5c7c8f35c8069ea1967fbade95130334585966d9
Ben McMann <bbmcmann@volosys.com>
2019-09-13 12:42:19 -0600

    Finish

--- chapter18/Check.java
+++ chapter18/Check.java
@@ -7,8 +7,20 @@ public class Check{
         Scanner input = new Scanner(System.in);
         double checkBalance, saveBalance;
 
-        
+        System.out.println("What is checking balance?");
+        checkBalance = input.nextDouble();
+        System.out.println("What is savings balance?");
+        saveBalance = input.nextDouble();
 
+        if(saveBalance < 1500.0 || checkBalance < 1000.0){
+
+            System.out.println("Charge is 0.15");
+
+        }
+        else{
+
+            System.out.println("no charge");
+        }
     }
 
 }
