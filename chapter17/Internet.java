3485e86b0a26a77e5877be8ed8cb854a1c33feda
Ben McMann <bbmcmann@volosys.com>
2019-09-13 12:13:02 -0600

    Finish

--- chapter17/Internet.java
+++ chapter17/Internet.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.scanner;
 
 public class Internet{
     public static void main(String[] args){
@@ -8,10 +7,25 @@ public class Internet{
         String order;
         double price;
         int delivery;
+        double deliverPrice = 0;
+
+        System.out.println("Enter item");
         order = input.nextLine();
+        System.out.println("Enter price");
         price = input.nextDouble();
+        System.out.println("Overnoght delivery = 1, no overnight = 0");
         delivery = input.nextInt();
         
+        if(price < 10){
+            deliverPrice += 2.00;
+        }
+        if(delivery == 1){
+            deliverPrice += 3.00;
+        }
+
+        System.out.println(order + " " + price);
+        System.out.println("Delivery: " + deliverPrice);
+        System.out.println("Total: " + (deliverPrice+price));
     }
 
 }
