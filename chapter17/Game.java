e9b37d15772fb1def3c75dbc6faf369ed20a51b3
Ben McMann <bbmcmann@volosys.com>
2019-09-13 12:27:16 -0600

    Finiah

--- chapter17/Game.java
+++ chapter17/Game.java
@@ -5,6 +5,23 @@ public class Game{
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
+        int strength, health, luck;
+
+        System.out.println("Write strength:");
+        strength = input.nextInt();
+        System.out.println("Write health:");
+        health = input.nextInt();
+        System.out.println("Write luck:");
+        luck = input.nextInt();
+
+        if(strength + health + luck > 15){
+            System.out.println("Too many points bro. Stop cheating");
+            strength = 5;
+            health = 5;
+            luck = 5;
+        }
+
+        System.out.println("Health: " +  health + " Strength: " + strength + " Luck: " + luck);
         
     }
 
