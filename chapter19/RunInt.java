4f9d7228c22d86869b5cedcfc2789828c4abf291
Ben McMann <bbmcmann@volosys.com>
2019-09-16 12:33:30 -0600

    Finish

--- chapter19/RunInt.java
+++ chapter19/RunInt.java
@@ -5,7 +5,19 @@ public class RunInt{
         
         Scanner input = new Scanner(System.in);
 
-        
+        int start, ending;
+
+        System.out.println("Enter starting number");
+        start = input.nextInt();
+        System.out.println("Enter ending number");
+        ending = input.nextInt();
+
+        while(start <= ending){
+
+            System.out.println(start);
+            start += 1;
+
+        }
 
     }
 
