cc3102ec62561f4c5f25c47f5cba7ba366021e09
Ben McMann <bbmcmann@volosys.com>
2019-09-16 12:41:58 -0600

    Finish

--- chapter19/Sum.java
+++ chapter19/Sum.java
@@ -4,8 +4,21 @@ public class Sum{
 
     public static void main(String[] args) {
         
-        
+        Scanner input = new Scanner(System.in);
+        int n, count, sum;
 
+        System.out.println("Give Me N");
+        n = input.nextInt();
+        count = 1;
+        sum = 0;
+
+        while(count <= n){
+            sum += count;
+            count += 1;
+        }
+
+        System.out.println(sum);
+        System.out.println((n*(n+1))/2);
     }
 
 }
