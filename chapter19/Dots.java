3f0eb5e74ec3aaf168b3f0c419b898bab0e582b2
Ben McMann <bbmcmann@volosys.com>
2019-09-16 12:54:41 -0600

    finish

--- chapter19/Dots.java
+++ chapter19/Dots.java
@@ -5,6 +5,24 @@ public class Dots{
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
+        String first, second;
+        int dots;
+
+        System.out.println("Give first word: ");
+        first = input.nextLine();
+        System.out.println("Give second word: ");
+        second = input.nextLine();
+
+        dots = 30 - (first.length() + second.length());
+
+        System.out.print(first);
+
+        while(dots > 0){
+            System.out.print(".");
+            dots -= 1;
+        }
+        
+        System.out.print(second);
 
     }
 
