f29aaa918acc539ee3f6012be7c32c60877e9cdd
Ben McMann <bbmcmann@volosys.com>
2019-09-17 12:08:48 -0600

    Finish

--- chapter20/Adding.java
+++ chapter20/Adding.java
@@ -5,6 +5,22 @@ public class Adding{
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
+        int numbers = 0;
+        int sum = 0;
+
+        System.out.println("How many numbers will be added?");
+        numbers = input.nextInt();
+
+        while(numbers > 0){
+
+            System.out.println("Enter a number: ");
+            sum += input.nextInt();
+
+            numbers -= 1;
+
+        }
+
+        System.out.println("Sum: " + sum);
 
     }
 
