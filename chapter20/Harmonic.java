0e99b6a16cfb58747d9929338b8c52d0335b3430
Ben McMann <bbmcmann@volosys.com>
2019-09-17 12:13:23 -0600

    Finish

--- chapter20/Harmonic.java
+++ chapter20/Harmonic.java
@@ -5,6 +5,21 @@ public class Harmonic{
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
+        double sum = 0.0;
+        int n;
+
+        System.out.println("Enter N");
+        n = input.nextInt();
+
+        while(n > 0){
+
+            sum += (1.0/n);
+
+            n-= 1;
+
+        }
+
+        System.out.println("Sum is: " + sum);
 
     }
 
