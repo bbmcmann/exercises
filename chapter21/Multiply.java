857d5f78422912cae2f8f8ea2850bd0e660a8b88
Ben McMann <bbmcmann@volosys.com>
2019-09-17 20:36:10 -0600

    Finish

--- chapter21/Multiply.java
+++ chapter21/Multiply.java
@@ -1,10 +1,31 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Multiply{
 
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        int score = 0;
+        int numOne, numTwo;
+
+        for(int i = 10; i >0; i--){
+            numOne = rand.nextInt(9);
+            numTwo = rand.nextInt(9);
+            System.out.println("What is " + numOne + " * " + numTwo);
+
+            if(input.nextInt() == numOne * numTwo ){
+                System.out.println("Correct!");
+                score ++;
+            }
+            else{
+                System.out.println("You are trash");
+            }
+
+        }
+
+        System.out.println(score);
 
     }
 
