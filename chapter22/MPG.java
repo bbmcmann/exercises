4d78078541171ae1b672028291aded6ede971a47
Ben McMann <bbmcmann@volosys.com>
2019-09-17 21:32:08 -0600

    Finish

--- chapter22/MPG.java
+++ chapter22/MPG.java
@@ -5,7 +5,38 @@ public class MPG{
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
+        int initMiles, finalMiles, gallons;
 
+        System.out.println("Enter Initial miles");
+        initMiles = input.nextInt();
+        System.out.println("Enter Final miles");
+        finalMiles = input.nextInt();
+        System.out.println("Enter gallons");
+        gallons = input.nextInt();
+
+        System.out.println("MPG:" + ((double)finalMiles -  initMiles)/(double)gallons);
+
+        while(initMiles >= 0){
+
+            System.out.println("Enter Initial miles");
+            initMiles = input.nextInt();
+            if(initMiles < 0){
+                System.out.println("BYE 1");
+                System.exit(0);
+
+            }
+
+            System.out.println("Enter Final miles");
+            finalMiles = input.nextInt();
+            
+            System.out.println("Enter gallons");
+            gallons = input.nextInt();
+
+            System.out.println("MPG: " + ((double)finalMiles -  initMiles)/(double)gallons);
+
+        }
+
+        System.out.println("BYE 2");
 
 
     }
