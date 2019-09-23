97058342c94b4d65c83a076faec175a36572b35c
Ben McMann <bbmcmann@volosys.com>
2019-09-23 12:27:06 -0600

    Finish

--- chapter24/Odds.java
+++ chapter24/Odds.java
@@ -5,7 +5,20 @@ public class Odds{
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
+        int count = 1; 
+        int sum = 0;
+        int n;
 
+        n = input.nextInt();
+
+        for( ; count <= n; count +=2){
+
+            sum += count;
+
+        }
+
+        System.out.println(sum);
+        System.out.println(n * n);
 
     }
 
