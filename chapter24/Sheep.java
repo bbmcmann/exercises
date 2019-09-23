954aade20b9928e12d649c2174ff702b9ce00fbc
Ben McMann <bbmcmann@volosys.com>
2019-09-23 12:49:11 -0600

    Finish

--- chapter24/Sheep.java
+++ chapter24/Sheep.java
@@ -6,10 +6,13 @@ public class Sheep{
         int t;
         double power = 0.83;
 
+        System.out.println(20);
+        
         for(t = 0; t < 26; t++){
+
             
-            power = power * 0.83;
             System.out.println(Math.round(220/(1 + 10 * power)));
+            power = power * 0.83;
 
         }
 
