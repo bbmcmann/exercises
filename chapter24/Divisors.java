1aa86fda00fcf46b98e8674bc0319b822848715d
Ben McMann <bbmcmann@volosys.com>
2019-09-24 12:22:08 -0600

    Finish

--- chapter24/Divisors.java
+++ chapter24/Divisors.java
@@ -4,7 +4,26 @@ public class Divisors{
 
     public static void main(String[] args){
 
-        
+        Scanner input = new Scanner(System.in);
+        int n, sum = 0;
+        n = input.nextInt();
+
+        for(int i = 1; i < n; i++){
+
+            if(n%i == 0){
+
+                sum += i;
+
+            }
+
+        }
+
+        if(sum == n){
+
+            System.out.println(n + " is a perfect number");
+
+        }
+        System.out.println(sum);
 
     }
 
