1be4b6818c1c33db17711615a8a437e17595dd35
Ben McMann <bbmcmann@volosys.com>
2019-09-24 12:48:41 -0600

    Finish

--- chapter25/Birthday.java
+++ chapter25/Birthday.java
@@ -5,9 +5,19 @@ public class Birthday{
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
+        int n; 
+        double probability;
 
+        n = input.nextInt();
+        probability = ((365-1)/(double)365);
 
+        for(int i =2; i <= n; i++ ){
 
+            probability = probability * ((365-i)/(double)365);
+
+        }
+
+        System.out.println(probability);
     }
 
 
