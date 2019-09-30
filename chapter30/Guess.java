347f9d67ca92ff82704f231572b8eb1b461b43e2
Ben McMann <bbmcmann@volosys.com>
2019-09-30 12:35:07 -0600

    Finish

--- chapter30/Guess.java
+++ chapter30/Guess.java
@@ -6,7 +6,24 @@ public class Guess{
         
         Random random = new Random();
         Scanner input = new Scanner(System.in);
+        int guess;
+        int x = random.nextInt(10) + 1;
+        
 
+        for (int i = 3; i > 0; i -- ){
+            System.out.println("Guess between 1 and 10");
+            guess = input.nextInt();
+
+            if(guess == x){
+                System.out.println("God job!");
+                System.exit(0);
+            }
+        }
+
+        System.out.println("You r trash.");
+        System.out.println("Answer was: " + x);
+
+        
     }
 
 
