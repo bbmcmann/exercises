abad666bfb99dcb49c30ec5139154e696ef4b613
Ben McMann <bbmcmann@volosys.com>
2019-09-30 12:26:26 -0600

    Initial Commit

---
+++ chapter30/Guess.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Guess{
+
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+    }
+
+
+}
