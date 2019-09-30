04ff775a54756607443b25ec9219f5c50324cf86
Ben McMann <bbmcmann@volosys.com>
2019-09-30 12:38:44 -0600

    Initial Commit

---
+++ chapter30/Improved.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Improved{
+
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+    }
+
+
+}
