4419c8e73bbc8f7ee603f446f08f1aa6eea7a87d
Ben McMann <bbmcmann@volosys.com>
2019-10-02 12:12:09 -0600

    Initial COmmit

---
+++ chapter30/SquareRoot.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SquareRoot{
+
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+        
+
+    }
+
+
+}
