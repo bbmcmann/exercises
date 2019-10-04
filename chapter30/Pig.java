af67e7f48b95647866ab845b38f1990e19e6b57c
Ben McMann <bbmcmann@volosys.com>
2019-10-04 12:19:10 -0600

    Finish

--- chapter30/Pig.java
+++ chapter30/Pig.java
@@ -9,39 +9,69 @@ public class Pig{
         int playerScore = 0; 
         int compScore = 0;
         int workSum = 0;
-        int playerRoll;
+        int playerRoll, round;
         String roll;
 
         boolean rolling = true;
         boolean gameOver = false;
 
+        round = 1;
         while(!gameOver){
 
+            System.out.println("**************");
+            System.out.println("ROUND: " + round);
+            System.out.println("**************");
             compScore += compScore + random.nextInt(6)+1 + random.nextInt(6)+1 + random.nextInt(6)+1;
+            System.out.println("Comp current Score: " + compScore);
+
+            if(compScore >= 100){
+                rolling = false;
+            }
+            else{
+                rolling = true;
+            }
             
             while(rolling){
-                System.out.println("Wanna quit? (y/n)");
-                roll = input.nextLine();
-
+                
                 playerRoll = random.nextInt(6) + 1;
+                System.out.println("Player roll: " + playerRoll);
 
                 if (playerRoll == 1){
+                    System.out.println("Roled 1, rip");
                     rolling = false;
                     workSum = 0;
                 }
                 else{
                     workSum = workSum + playerRoll;
+                    System.out.println("Didnt roll 1, sum is: " + workSum);
                 }
+
+                System.out.println("Wanna quit? (y/n)");
+                roll = input.nextLine();
 
                 if(roll.equals("y")){
                     rolling = false;
                 }
 
             }
+
             playerScore += workSum;
+            System.out.println("_________________");
+            System.out.println("Player Score: " + playerScore);
+            System.out.println("Comp score: " + compScore);
 
-
+            if(compScore >= 100){
+                gameOver = true;
+            }
+            else if(playerScore >= 100){
+                gameOver = true;
+            }
+            round ++;
         }
+        System.out.println("______________________");
+        System.out.println("Player Final Score: " + playerScore);
+        System.out.println("Comp Final Score: " + compScore);
+
 
     }
 
