4784b93159daf8a309caea458c1753781de2c0b6
Ben McMann <bbmcmann@volosys.com>
2019-10-23 13:09:51 -0600

    Fin

--- chapter40/Cylinder.java
+++ chapter40/Cylinder.java
@@ -1,3 +1,35 @@
 public class Cylinder{
-    
+
+    private double height;
+    private double radius;
+
+    public Cylinder(double radius, double height){
+        this.height = height;
+        this.radius = radius;
+    }
+
+    public double volume(){ // needs to be changed
+        return (2*Math.PI*radius*radius) + (2*Math.PI*radius*height);
+    }
+
+    public double surfaceArea(){
+        return Math.PI*radius*radius*height;
+    }
+
+    public void setHeight(){
+        this.height = height;
+    }
+
+    public void setRadius(){
+        this.radius = radius;
+    }
+
+    public double getHeight(){
+        return this.height;
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+
 }
