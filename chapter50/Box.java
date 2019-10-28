3426759fc282dcd11e8e5d709e0038ec47fc0696
Ben McMann <bbmcmann@volosys.com>
2019-10-28 13:10:14 -0600

    Constructors

--- chapter50/Box.java
+++ chapter50/Box.java
@@ -1,11 +1,24 @@
 class Box{
 
+    private double width;
+    private double height;
+    private double length;
+    private double side;
+
     public Box(double width, double height, double length){
+
+        this.width = width;
+        this.height = height;
+        this.length = length;
 
     }
 
     public Box(double side){
 
+        this.side = side;
+
     }
+
+    
 
 }
