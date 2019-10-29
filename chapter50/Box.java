88f208322efc04b52d5053552043c01bd8a01abc
Ben McMann <bbmcmann@volosys.com>
2019-10-29 12:20:11 -0600

    Set up methods and testing

--- chapter50/Box.java
+++ chapter50/Box.java
@@ -4,25 +4,63 @@ class Box{
     private double height;
     private double length;
     private double side;
+    private boolean cube;       //to know what constructor was used in main
 
     public Box(double width, double height, double length){
 
         this.width = width;
         this.height = height;
         this.length = length;
+        this.cube = false;
 
     }
 
     public Box(double side){
 
         this.side = side;
+        this.cube = true;
 
+    }
+
+    public double getLength(){
+        if(cube){
+            return side;
+        }
+        return this.length;
+    }
+
+    public double getHeight(){
+        if(cube){
+            return side;
+        }
+        return this.height;
+    }
+
+    public double getWidth(){
+        if(cube){
+            return side;
+        }
+        return this.width;
+    }
+
+    public double surfaceArea(double s1, double s2){
+        return (facePairArea(width, height)) + (facePairArea(height, length)) + (facePairArea(length, width));
     }
 
     public double volume(){
+        if(cube){
+            return side*side*side;
+        }
         return width*height*length;
     }
 
+    public String toString(){
+        return "";
+    }
+
+    private double facePairArea(double s1, double s2){
+        return s1 * s2 * 2;
+    }
     
 
 }
--- chapter50/BoxTester.java
+++ chapter50/BoxTester.java
@@ -4,6 +4,9 @@ class BoxTester
   public static void main ( String[] args )
   {
      
+    Box box = new Box(2.5,5.0,6.0);
+
+    System.out.println("");
 
   }
 }
