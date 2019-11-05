99eb558b9d9de415d944df8f21a26d91ba2ae754
Ben McMann <bbmcmann@volosys.com>
2019-11-05 12:53:45 -0700

    Compiles, does not work

--- chapter54/Odious.java
+++ chapter54/Odious.java
@@ -4,7 +4,36 @@ public class Odious{
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
+        int number;
 
+        System.out.println("Enter a number bro.");
+        number = input.nextInt();
+
+        while(number > 0){
+            if(findBinaryOnes(number) % 2 == 1){
+                System.out.println("Its ODIOUS");
+            }
+            else{
+                System.out.println("Its evil.");
+            }
+            System.out.println("Enter a number bro.");
+            number = input.nextInt();
+        }
+
+    }
+
+    public static int findBinaryOnes(int number){
+        int numOfOnes = 0;
+
+        while(number != 0){
+            number = number % 2;
+            if(number == 1){
+                numOfOnes ++;
+            }
+            number = number/2;
+        }
+
+        return numOfOnes;
     }
 
 }
