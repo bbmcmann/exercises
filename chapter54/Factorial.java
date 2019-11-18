3a5fd99f12c8971c75d4f36cc4b96763c85dbd6e
Ben McMann <bbmcmann@volosys.com>
2019-11-18 12:25:59 -0700

    Fin

--- chapter54/Factorial.java
+++ chapter54/Factorial.java
@@ -1,11 +1,16 @@
-import java.util.scanner;
+import java.util.Scanner;
+
 
 public class Factorial{
 
     public static long factorial(int n){
-        long total = 0;
-        while(n > 1){
-            total += n * n-1;
+        long total = 1;
+        if(n > 20 || n < 0){
+            return -1;
+        }
+        while(n > 0){
+            total *= n;
+            n--;
         }
         return total;
     }
@@ -13,7 +18,18 @@ public class Factorial{
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-
+        int n;
+        long fact;
+        System.out.println("Enter a number to be !d");
+        n = input.nextInt();
+        fact = factorial(n);
+        if(fact < 0){
+            System.out.println(factorial(n));
+            System.out.println("***********************ERROR OUT OF RANGE***********************");
+        }
+        else{
+            System.out.println(factorial(n));
+        }
 
     }
 
