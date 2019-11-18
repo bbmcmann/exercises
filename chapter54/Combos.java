bc5a85497f93eb549e4a85c866dee39fe06bf3d4
Ben McMann <bbmcmann@volosys.com>
2019-11-18 12:29:29 -0700

    Add Factorial Function

--- chapter54/Combos.java
+++ chapter54/Combos.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class Combos{
 
+    public static long factorial(int n){
+        long total = 1;
+        if(n > 20 || n < 0){
+            return -1;
+        }
+        while(n > 0){
+            total *= n;
+            n--;
+        }
+        return total;
+    }
+
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
