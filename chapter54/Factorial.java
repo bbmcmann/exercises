4d0e0bbf0757a120cf892ef9e135dfb9e107a5ad
Ben McMann <bbmcmann@volosys.com>
2019-11-18 12:16:49 -0700

    Setup Factorial

--- chapter54/Factorial.java
+++ chapter54/Factorial.java
@@ -2,11 +2,19 @@ import java.util.scanner;
 
 public class Factorial{
 
+    public static long factorial(int n){
+        long total = 0;
+        while(n > 1){
+            total += n * n-1;
+        }
+        return total;
+    }
+
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
 
-        
+
     }
 
 }
