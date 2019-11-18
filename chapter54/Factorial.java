64c7c2f2df01b228fe6a524654df351a69db288e
Ben McMann <bbmcmann@volosys.com>
2019-11-18 12:12:30 -0700

    Initial

---
+++ chapter54/Factorial.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+
+public class Factorial{
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        
+    }
+
+}
