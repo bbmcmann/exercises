bd9fa18c4bdd4be09b06c3244a9354993ab8c630
Ben McMann <bbmcmann@volosys.com>
2019-11-19 12:37:33 -0700

    Init

---
+++ chapter60/ThreeArrays.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+
+class ThreeArrays{
+
+    public static void main(String[] args) {
+        int[] valA = {13,-22,82,17};
+        int[] valB = {-12,24,-79,-13};
+        int[] sum = {0,0,0,0};
+
+        
+        
+        System.out.println(Arrays.toString(sum));
+    }
+}
