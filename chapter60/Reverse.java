228c015d2f8533884f32132be6e6fbd87d5bcf5e
Ben McMann <bbmcmann@volosys.com>
2019-11-19 12:51:20 -0700

    Init

---
+++ chapter60/Reverse.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Reverse{
+    public static void main(String[] args) {
+        int[] val = {0,1,2,3};
+        int temp;
+
+        System.out.println(Arrays.toString(val));
+
+        //reverse it
+
+
+        System.out.println(Arrays.toString(val));
+    }
+}
