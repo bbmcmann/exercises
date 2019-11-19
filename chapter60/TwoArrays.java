d51a5e35b811096641caa4cb5e02cc228600dba5
Ben McMann <bbmcmann@volosys.com>
2019-11-19 12:32:11 -0700

    Import Arrays to use toString()

---
+++ chapter60/TwoArrays.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class TwoArrays{
+
+    public static void main(String[] args) {
+        
+        int[] val = {13,-4,82,17};
+        int[] twice;
+
+        System.out.println(Arrays.toString(val));
+
+        twice = new int[4];
+
+        System.out.println(Arrays.toString(twice));
+        
+    }
+
+}
