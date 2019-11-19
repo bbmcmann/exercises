dd7dc80f6164c4ce157dd7665df34b2a6ee59834
Ben McMann <bbmcmann@volosys.com>
2019-11-19 12:42:17 -0700

    Init

---
+++ chapter60/SameSum.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class SameSum{
+
+
+    public static void main(String[] args) {
+        
+        int[] valA = {13,-22,82,17};
+        int[] valB = {0,0,0,0};
+        int[] vsum = new int[4];
+
+        System.out.println(Arrays.toString(valA));
+        System.out.println(Arrays.toString(valB));
+        System.out.println(Arrays.toString(sum));
+        
+    }
+
+
+}
