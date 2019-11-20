6074ab910ea25774e12f0501e19fcdee9be56027
Ben McMann <bbmcmann@volosys.com>
2019-11-20 13:01:00 -0700

    Fin

---
+++ chapter61/Sums.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Sums{
+
+    public static void main(String[] args) {
+        
+        int[] data = {3,2,5,7,9,12,97,24,54};
+
+        for(int index = 0; index < data.length; index++){
+
+            
+        }
+
+    }
+}
