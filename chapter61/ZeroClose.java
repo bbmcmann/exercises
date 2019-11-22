5bc49c04256dea321bf0ae388fe7f1c1860a98b3
Ben McMann <bbmcmann@volosys.com>
2019-11-22 12:29:48 -0700

    Fin

--- chapter61/ZeroClose.java
+++ chapter61/ZeroClose.java
@@ -1,19 +1,18 @@
-import java.util.io.*;
+import java.io.*;
 
 public class ZeroClose{
 
     public static void main(String[] args) {
      
-        int[] data = {3, 1, 5, 7, 4, 12, -3, 8, -2};
-    
-        //for (  )
-        //{
-        //}
-          
-  
-        System.out.println(  );
-       
-
+        int[] data = {3, -1, 5, 7, 4, 12, -3, 8, -2};
+        int closest = data[0];
+        for(int i = 0; i < data.length; i++){
+            if(Math.abs(data[i]) < closest){
+                closest = data[i];
+            }
+        }
+        
+        System.out.println(closest);
     }
 
 }
