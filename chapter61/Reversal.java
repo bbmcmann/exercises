23896a21ae2b724b0dcca3bb72597f65ca443d9e
Ben McMann <bbmcmann@volosys.com>
2019-11-22 12:33:48 -0700

    Init

---
+++ chapter61/Reversal.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.io.*;
+
+public class Reveral{
+
+    public static void main(String[] args) {
+     
+        int[] data = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+    
+        // reverse the data
+        for ( int i = 0; i < data.length/2; i++){
+
+        }
+      
+        // write out the new data
+        for ( int i=0; i < data.length; i++){
+
+        }
+
+    }
+
+}
