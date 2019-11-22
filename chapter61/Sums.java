a2f68c0255de7eea37b139bc468146170dd8e916
Ben McMann <bbmcmann@volosys.com>
2019-11-22 12:15:14 -0700

    Fin

--- chapter61/Sums.java
+++ chapter61/Sums.java
@@ -5,10 +5,23 @@ class Sums{
     public static void main(String[] args) {
         
         int[] data = {3,2,5,7,9,12,97,24,54};
+        int sum = 0;
+        int evens = 0;
+        int odds = 0;
 
-        for(int index = 0; index < data.length; index++){
-            
+        for(int i = 0; i < data.length; i++){
+            sum += data[i];
+            if(data[i]%2 == 0){
+                evens += data[i];
+            }
+            else{
+                odds += data[i];
+            }
         }
+
+        System.out.println(sum);
+        System.out.println(evens);
+        System.out.println(odds);
 
     }
 }
