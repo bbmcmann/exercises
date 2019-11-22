20dcc9251d8308d0be9a3f756a8c06f92edb24e9
Ben McMann <bbmcmann@volosys.com>
2019-11-22 12:24:05 -0700

    Init

---
+++ chapter61/ZeroClose.java
@@ -0,0 +1,19 @@
+import java.util.io.*;
+
+public class ZeroClose{
+
+    public static void main(String[] args) {
+     
+        int[] data = {3, 1, 5, 7, 4, 12, -3, 8, -2};
+    
+        //for (  )
+        //{
+        //}
+          
+  
+        System.out.println(  );
+       
+
+    }
+
+}
