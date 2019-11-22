3e6d2989b8031525eecad35e868a714dde847148
Ben McMann <bbmcmann@volosys.com>
2019-11-22 12:22:33 -0700

    Fin

--- chapter61/TwoLarge.java
+++ chapter61/TwoLarge.java
@@ -5,14 +5,21 @@ class TwoLarge{
     public static void main(String[] args) {
         
         int[] data = {3, 1, 5, 7, 4, 12, -3, 8, -2};
-    
-    
-    
-        for ( int i = 0; i < data.length; i++){
-            
+        int largest = data[0];
+        int secondLarge = data[0];
+
+        for (int i = 0; i < data.length; i++){
+            if(data[i] > largest){
+                secondLarge = largest;
+                largest = data[i];
+            }
+            else if(data[i] > secondLarge){
+                secondLarge = data[i];
+            }
         }
       
-        System.out.println(  );
+        System.out.println(largest);
+        System.out.println(secondLarge);
 
     }
 
