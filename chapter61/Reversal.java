b1db067032d81fc2c08249a91a9507e689ddd097
Ben McMann <bbmcmann@volosys.com>
2019-11-22 12:37:59 -0700

    1st Part

--- chapter61/Reversal.java
+++ chapter61/Reversal.java
@@ -1,21 +1,21 @@
 import java.util.Arrays;
 import java.io.*;
 
-public class Reveral{
+public class Reversal{
 
     public static void main(String[] args) {
      
         int[] data = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-    
-        // reverse the data
-        for ( int i = 0; i < data.length/2; i++){
+        int temp;
 
+        for(int i = 0; i < data.length/2; i++){
+            int swapSpot = data.length-1-i;
+            temp = data[i];
+            data[i] = data[swapSpot];
+            data[swapSpot] = temp;
         }
       
-        // write out the new data
-        for ( int i=0; i < data.length; i++){
-
-        }
+        System.out.println(Arrays.toString(data));
 
     }
 
