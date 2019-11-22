42ff2545e1df35dff5258aa522c3f41a2610e4e4
Ben McMann <bbmcmann@volosys.com>
2019-11-22 12:50:39 -0700

    init

--- chapter61/Reversal2.java
+++ chapter61/Reversal2.java
@@ -6,13 +6,10 @@ public class Reversal2{
     public static void main(String[] args) {
      
         int[] data = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-        int temp;
+        int[] reversedData = new int[14];
 
-        for(int i = 0; i < data.length/2; i++){
-            int swapSpot = data.length-1-i;
-            temp = data[i];
-            data[i] = data[swapSpot];
-            data[swapSpot] = temp;
+        for(int i = 0; i < data.length; i++){
+            reversedData[i] = data[data.length-1-i];
         }
       
         System.out.println(Arrays.toString(data));
