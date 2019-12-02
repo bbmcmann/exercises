e0183f1894271104542b48538be796fa6bf30db3
Ben McMann <bbmcmann@volosys.com>
2019-12-02 12:50:20 -0700

    Set up

--- chapter61/Equality.java
+++ chapter61/Equality.java
@@ -2,8 +2,31 @@ import java.util.Arrays;
 import java.io.*;
 
 public class Equality{
-    public static void main(String[] args) {
-        
-    }
 
+    public static boolean myEquals(int[] a,int[] b){
+        boolean equal = true;
+
+        return equal;
+    }
+    public static void main(String[] args) {
+        int[] arrayA = { 1, 2, 3, 4 };
+        int[] arrayB = { 1, 2, 3, 4 };
+
+        System.out.print("Arrays says: ");
+        if (Arrays.equals(arrayE, null)){
+            System.out.println("Equal");
+        }
+        else{
+            System.out.println("NOT Equal");      
+        }
+
+        System.out.print("myEquals says: ");
+
+        if (Arrays.equals(arrayE, null)){
+            System.out.println( "Equal" );
+        }
+        else{
+            System.out.println("NOT Equal");
+        }
+    }
 }
