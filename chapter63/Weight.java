5f15781046414dbea7750c001a1667e1e56565c6
Ben McMann <bbmcmann@volosys.com>
2019-12-02 22:17:18 -0700

    Fin

--- chapter63/Weight.java
+++ chapter63/Weight.java
@@ -4,20 +4,18 @@ class Weight{
     private int[] data;
     
     // Constructor
-    public Weight(int[] init)
-    {
-
-      int[] data = new int[init.length];
+    public Weight(int[] init){
+      data = new int[init.length];
       
- 
-      for(int i = 0; i < data.length; i ++){
+      for(int i = 0; i < init.length; i ++){
+        //System.out.println(init[i]);
         data[i] = init[i];
+        //System.out.println(data[i]);
       }
     }
     
     //Print
     public String toString(){
         return Arrays.toString(data);
-   
     }
 }
